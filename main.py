b7bf8e9776eaeb2f9bae15ee8d8ff125c3f7969e
James <jcarstens@unomaha.edu>
2022-12-05 19:07:51 -0600

    Added snake movement and food positioning

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import pygame
+import random
+
+pygame.init()
 
 WIDTH, HEIGHT = 595, 595
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Project 2 - Snake")
-FPS = 60
+FPS = 4
 GRAY = (100, 100, 100)
 BLACK = (0, 0, 0)
-SNAKE_VEL = 5
+COLOR = (100,100,0)
 
 
-# def handle_snake(key_pressed, snake_list):
-#     for x in snake_list:
-#         pygame.draw.rect(WIN, BLACK, )
+def handle_snake(snake_direction, snake_head):
+    if snake_direction == 'up':
+        snake_head[1] -= 1
+    if snake_direction == 'down':
+        snake_head[1] += 1
+    if snake_direction == 'left':
+        snake_head[0] -= 1
+    if snake_direction == 'right':
+        snake_head[0] += 1
 
 
-def draw_window(snake_list):
+def draw_window(snake_head, snake_list, fruit_location):
     WIN.fill(GRAY)
     block_size = 35
     for x in range(0, WIDTH, block_size):
@@ -22,19 +31,26 @@ def draw_window(snake_list):
             rect = pygame.Rect(x, y, block_size, block_size)
             pygame.draw.rect(WIN, BLACK, rect, 1)
 
-    x, y = snake_list[0]
+    x, y = fruit_location
     rect = pygame.Rect(x * block_size, y * block_size, block_size, block_size)
     pygame.draw.rect(WIN, BLACK, rect)
 
-    for x, y in snake_list[1:]:
-        rect = pygame.Rect(x * block_size, y * block_size, block_size, block_size)
+    for x in snake_list:
+        rect = pygame.Rect(x[0] * block_size, x[1] * block_size, block_size, block_size)
         pygame.draw.rect(WIN, BLACK, rect)
+
+    x, y = snake_head
+    rect = pygame.Rect(x * block_size, y * block_size, block_size, block_size)
+    pygame.draw.rect(WIN, BLACK, rect)
 
     pygame.display.update()
 
 
 def main():
-    snake_list = [(8, 8), (7, 8), (6, 8)]
+    snake_head = [8, 8]
+    snake_list = [[8, 8], [7, 8], [7, 8]]
+    fruit_location = [random.randrange(1, 17), random.randrange(1, 17)]
+    snake_direction = "right"
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -42,10 +58,21 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        key_pressed = pygame.key.get_pressed()
-        #handle_snake(key_pressed, snake_list)
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP and snake_direction != "down":
+                    snake_direction = "up"
+                if event.key == pygame.K_DOWN and snake_direction != "up":
+                    snake_direction = "down"
+                if event.key == pygame.K_LEFT and snake_direction != "right":
+                    snake_direction = "left"
+                if event.key == pygame.K_RIGHT and snake_direction != "left":
+                    snake_direction = "right"
 
-        draw_window(snake_list)
+        handle_snake(snake_direction, snake_head)
+        snake_list.insert(0, list(snake_head))
+        snake_list.pop()
+        draw_window(snake_head, snake_list, fruit_location)
+
 
 
 if __name__ == '__main__':
