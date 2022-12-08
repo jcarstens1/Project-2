81a0144d793f29436395a56e1b08c1fb5d890c3b
James <jcarstens@unomaha.edu>
2022-12-07 19:18:58 -0600

    Changed game colors

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Project 2 - Snake")
 FPS = 8
 
-GRAY = (100, 100, 100)
-BLACK = (0, 0, 0)
+GRAY = (30, 30, 30)
+YELLOW =(149, 156, 34)
 RED = (200, 0, 0)
-BLUE = (0, 0 , 100)
-LIGHT_BLUE = (0, 0, 50)
+LIGHT_BLUE = (36, 100, 128)
+PURPLE = (96, 60, 133)
+DARK_PURPLE = (86, 27, 99)
+BLUE = (27, 75, 96)
 
 score_font = pygame.font.SysFont('lucidasanstypewriteroblique', 20)
 
@@ -20,7 +22,7 @@ score_font = pygame.font.SysFont('lucidasanstypewriteroblique', 20)
 #line 23 puts the score onto the Window
 def score(score):
     score = score - 2
-    player_score = score_font.render('Your Score: ' + str(score), True, BLUE)
+    player_score = score_font.render('Your Score: ' + str(score), True, RED)
     WIN.blit(player_score, [10,10])
 
 def handle_snake(snake_direction, snake_head):
@@ -35,23 +37,23 @@ def handle_snake(snake_direction, snake_head):
 
 
 def draw_window(block_size, snake_head, snake_list, fruit_location):
-    WIN.fill(GRAY)
+    WIN.fill(LIGHT_BLUE)
     for x in range(0, WIDTH, block_size):
         for y in range(0, HEIGHT, block_size):
             rect = pygame.Rect(x, y, block_size, block_size)
-            pygame.draw.rect(WIN, BLACK, rect, 1)
+            pygame.draw.rect(WIN, GRAY, rect, 1)
 
     x, y = fruit_location
     rect = pygame.Rect(x * block_size, y * block_size, block_size, block_size)
-    pygame.draw.rect(WIN, RED, rect)
+    pygame.draw.rect(WIN, YELLOW, rect)
 
     for x in snake_list[1:]:
         rect = pygame.Rect(x[0] * block_size, x[1] * block_size, block_size, block_size)
-        pygame.draw.rect(WIN, BLUE, rect)
+        pygame.draw.rect(WIN, PURPLE, rect)
 
     x, y = snake_head
     rect = pygame.Rect(x * block_size, y * block_size, block_size, block_size)
-    pygame.draw.rect(WIN, LIGHT_BLUE, rect)
+    pygame.draw.rect(WIN, DARK_PURPLE, rect)
     score(len(snake_list) - 1)
 
     pygame.display.update()
