6c5fb0eee8b40a30cc8ec2b48fbb5528f8bb2f07
James <jcarstens@unomaha.edu>
2022-12-08 14:57:32 -0600

    Shows score after snake died and added sound

--- main.py
+++ main.py
@@ -1,27 +1,23 @@
 import pygame
 import random
+import os
 import pygame_menu
 from pygame_menu import Theme
 
-#TODO: finish final_screen and the logic
 
 def play_game():
     main()
 
-#inputs player's score len(snake_list)
-def final_screen(player_score):
-    if player_score == 289:
-        #add winner screen
-        pass
-    else:
-        #loser screen
-        pass
+def show_score(score):
+    LOSE_SOUND.play()
+    WIN.fill(LIGHT_BLUE)
+    final_font = pygame.font.SysFont('8BIT', 50)
+    player_score = final_font.render('Your Score: ' + str(score), True, RED)
+    WIN.blit(player_score, [WIDTH//3 - 15,HEIGHT//3])
+    pygame.display.update()
+    pygame.time.delay(3000)
+    return False
 
-#inputs len(snake_list) and covnerts to string
-#line 23 puts the score onto the Window
-def player_score(score):
-    player_score = score_font.render('Your Score: ' + str(score), True, RED)
-    WIN.blit(player_score, [10,10])
 
 def handle_snake(snake_direction, snake_head):
     if snake_direction == 'up':
@@ -34,7 +30,7 @@ def handle_snake(snake_direction, snake_head):
         snake_head[0] += 1
 
 
-def draw_window(block_size, snake_head, snake_list, fruit_location):
+def draw_window(block_size, snake_head, snake_list, fruit_location, score):
     WIN.fill(LIGHT_BLUE)
     for x in range(0, WIDTH, block_size):
         for y in range(0, HEIGHT, block_size):
@@ -52,7 +48,9 @@ def draw_window(block_size, snake_head, snake_list, fruit_location):
     x, y = snake_head
     rect = pygame.Rect(x * block_size, y * block_size, block_size, block_size)
     pygame.draw.rect(WIN, DARK_PURPLE, rect)
-    player_score(len(snake_list) - 1)
+
+    player_score = score_font.render('Your Score: ' + str(score), True, RED)
+    WIN.blit(player_score, [10,10])
 
     pygame.display.update()
 
@@ -63,6 +61,8 @@ def fruit(snake_list):
             fruit_location = [random.randrange(0, 17), random.randrange(0, 17)]
         return fruit_location
 
+    
+
 def main():
     block_size = 34
     snake_head = [8, 8]
@@ -72,10 +72,11 @@ def main():
     clock = pygame.time.Clock()
     run = True
     while run:
+        score = len(snake_list) - 1
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run = False
+                pygame.quit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP and snake_direction != "down":
                     snake_direction = "up"
@@ -86,31 +87,26 @@ def main():
                 if event.key == pygame.K_RIGHT and snake_direction != "left":
                     snake_direction = "right"
 
-        if snake_head[0] * block_size >= WIDTH:
-            run = False
-        if snake_head[0] * block_size < 0:
-            run = False
-        if snake_head[1] * block_size >= HEIGHT:
-            run = False
-        if snake_head[1] * block_size < 0:
-            run = False
 
         handle_snake(snake_direction, snake_head)
         snake_list.insert(0, list(snake_head))
         snake_list.pop()
 
+        if snake_head[0] * block_size >= WIDTH or snake_head[0] * block_size < 0 or snake_head[1] * block_size >= HEIGHT or snake_head[1] * block_size < 0:
+            run = show_score(score)
+
         for coord in snake_list[1:]:
             if snake_head[0] == coord[0] and snake_head[1] == coord[1]:
-                run = False
+                run = show_score(score)
 
         #checks to see if snake_head is in the same location fruit_location
         #if the location matches, then a snake_piece is added and fruit is randomized again
         if (snake_head[0] * block_size == fruit_location[0] * block_size) and (snake_head[1] * block_size == fruit_location[1] * block_size):
+            MUNCH_SOUND.play()
             snake_list.append(fruit_location)
             fruit_location = fruit(snake_list)
-        player_score(len(snake_list))
         
-        draw_window(block_size, snake_head, snake_list, fruit_location)
+        draw_window(block_size, snake_head, snake_list, fruit_location, score)
 
 pygame.init()
 
@@ -119,7 +115,7 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Project 2 - Snake")
 FPS = 8
 
-BLACK = (0,0,0)
+BLACK = (1,1,1)
 GRAY = (30, 30, 30)
 YELLOW =(149, 156, 34)
 RED = (200, 0, 0)
@@ -127,11 +123,13 @@ LIGHT_BLUE = (36, 100, 128)
 PURPLE = (96, 60, 133)
 DARK_PURPLE = (86, 27, 99)
 BLUE = (27, 75, 96)
+LOSE_SOUND = pygame.mixer.Sound(os.path.join('assets', "bruh.mp3"))
+MUNCH_SOUND = pygame.mixer.Sound(os.path.join('assets', "munch.mp3"))
 
 score_font = pygame.font.SysFont('lucidasanstypewriteroblique', 20)
 
-my_theme = Theme(background_color=BLACK,
-    #title_bar_style=pygame_menu.widgets.MENUBAR_STYLE_NONE,
+my_theme = Theme(background_color=LIGHT_BLUE,
+    title_bar_style=pygame_menu.widgets.MENUBAR_STYLE_NONE,
     #if you uncomment, there will be a warning thrown, but there will be
     #no gray bar along the top
     title_font=pygame_menu.font.FONT_8BIT,
@@ -140,6 +138,7 @@ my_theme = Theme(background_color=BLACK,
     widget_font=pygame_menu.font.FONT_MUNRO,
     widget_border_color=PURPLE,
     widget_padding=10
+    
 )
 menu = pygame_menu.Menu(
     height=HEIGHT,
@@ -152,4 +151,7 @@ menu.add.button('Play', play_game)
 menu.add.button('Quit', pygame_menu.events.EXIT)
 
 if __name__ == '__main__':
-    menu.mainloop(WIN)
+    try:
+        menu.mainloop(WIN)
+    except:
+        print("Goodbye")
